36cdb8cb68438db0edb7bf7cb2d949ca9c2736cd
stranger <2633565580@qq.com>
2024-04-08 11:25:39 +0800

    docs(helper): simplify comment

--- toolbox/helper.go
+++ toolbox/helper.go
@@ -5,41 +5,7 @@ import (
 	"strings"
 )
 
-// ToFirstLetterUpper 第一个字母大写
-func ToFirstLetterUpper(str string) string {
-	if len(str) == 0 {
-		return str
-	}
-	return strings.ToUpper(str[:1]) + str[1:]
-}
-
-// Mkdir
-// @Date 2023-02-17 17:55:04
-// @Param dir string
-// @Return error
-// @Description: 创建一个目录
-func Mkdir(dir string) error {
-	// 先尝试打开目录
-	if _, err := os.Open(dir); os.IsNotExist(err) {
-		// 如果文件不存在则创建目录
-		if err := os.Mkdir(dir, os.ModeDir); err != nil {
-			return err
-		} else {
-			return nil
-		}
-	} else if err != nil {
-		return err
-	} else {
-		return nil
-	}
-}
-
-// openFile
-// @Date 2023-02-17 17:55:15
-// @Param path string
-// @Return *os.File
-// @Return error
-// @Description: 打开一个文件
+// openFile 打开一个文件
 func openFile(path string) (*os.File, error) {
 	// 先尝试打开一个文件
 	if open, err := os.OpenFile(path, os.O_RDWR, os.ModePerm); os.IsNotExist(err) {
@@ -55,11 +21,7 @@ func openFile(path string) (*os.File, error) {
 	}
 }
 
-// EscapeRegxPath
-// @Date 2023-02-17 19:07:53
-// @Param path string
-// @Return string
-// @Description: regx脚本转义
+// EscapeRegxPath regx脚本转义
 func EscapeRegxPath(path string) string {
 	return strings.ReplaceAll(path, `\`, `\\`)
 }
